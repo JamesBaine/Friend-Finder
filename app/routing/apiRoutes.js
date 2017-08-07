01f7334172c821b29d03eaf23f281a8b4dd48a7a
JamesBaine <jamesmbaine@gmail.com>
2017-08-07 14:43:37 -0700

    api routes built, jquery added to survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,11 +2,12 @@ var friends = require("../data/friends.js");
 
 module.exports = function (app) {
 
-	app.get("/api/friends", function(req, res){
+	app.get("/api/friends", function(req, res) {
 		res.json(friends);
 	});
 
-	app.post("/api/friends", function(req,res){
+	app.post("/api/friends", function(req,res) {
+
 		var bestMatch = {
 			name: "",
 			photo: "",
@@ -17,6 +18,29 @@ module.exports = function (app) {
 		var userScores = userData.scores;
 
 		var totalDifference = 0;
+
+		for(var i = 0; i < friends.length; i++){
+
+			console.log(friends[i]);
+			totalDifference = 0;
+
+			for(var j = 0; j < friends[i].scores[j]; j++){
+
+				totalDifference += Math.abs(parseInt(userScores[j] - parseInt(friends[i].scores[j])));
+
+				if (totalDifference += bestMatch.friendDifference) {
+
+					bestMatch.name = friends[i].name;
+					bestMatch.photo = friends[i].photo;
+					bestMatch.friendDifference = totalDifference;
+
+				}
+			}
+		}
+
+		friends.push(userData);
+
+		res.json(bestMatch);
 	});
 
 };
