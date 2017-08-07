49572d0cf9a1a549b8d865b974c690b325bfa303
JamesBaine <jamesmbaine@gmail.com>
2017-08-07 13:00:47 -0700

    front-end done

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+
+	{
+		name: "Seymour Butts",
+		photo: "http://lorempixel.com/output/sports-q-c-400-400-5.jpg",
+		scores: [
+			"5",
+			"1",
+			"2",
+			"4",
+			"3",
+			"3",
+			"1",
+			"2",
+			"3",
+			"5"
+		]
+	},{
+		name: "Suq Madiq",
+		photo: "http://lorempixel.com/output/food-q-c-400-400-10.jpg",
+		scores: [
+			"3",
+			"4",
+			"5",
+			"1",
+			"3",
+			"3",
+			"5",
+			"5",
+			"2",
+			"5"
+		]
+	},{
+		name: "Mike Hunt",
+		photo: "http://lorempixel.com/output/fashion-q-c-400-400-9.jpg",
+		scores: [
+			"4",
+			"4",
+			"5",
+			"5",
+			"3",
+			"3",
+			"4",
+			"1",
+			"1",
+			"1"
+		]
+	},
+
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: "text/html" }));
 
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 });
